57c0e8f478cfe59f9a4af01ee0ae936e1256a46a
Baxter Langlar <blanglar@optusnet.com.au>
2019-04-17 05:03:43 +0000

    can't figure out how to scanf whole message

--- Assignment.c
+++ Assignment.c
@@ -2,13 +2,13 @@
 
 int main(){
 /* Reading hello from input1 file*/
-     FILE *input;
-     input = fopen("input1", "r");
-     if(input == NULL) {
+     FILE *input1;
+     input1 = fopen("input1", "r");
+     if(input1 == NULL) {
          perror("fopen()");       
      }
      char word[5];
-     fscanf(input,"%s",word);
+     fscanf(input1,"%s",word);
      printf("You entered the word: %s\n",word);
      printf("The word %s with a rotation cipher of 2 is ",word);
 /* Rotation of 2 Cipher*/
@@ -21,5 +21,24 @@ int main(){
         printf("%c",word[i]);
     }
 /* Decryption of roatation cipher*/
+printf("\n");
+    FILE * input2;
+    input2 = fopen("input2","r");
+    if(input2 == NULL){
+        perror("fopen()");
+    }
+    char mess[511];
+    fscanf(input2,"%s",mess);
+    /*int i2;
+    for(i2=0;i2<511;i++){
+       if(mess[i] == NULL){
+        printf(" "); 
+        }
+    printf("%c",mess[i]);
+    }*/
+    printf("%s",mess);
+    printf("hisdkasd");
+    
+    
 return 0;
 }
